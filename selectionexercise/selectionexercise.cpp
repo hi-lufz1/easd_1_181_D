4e29a698caf556a82f4373f41710caf2326b22d4
hi-lufz1 <latifufauzi@gmail.com>
2023-04-07 09:25:56 +0700

    no 1

--- selectionexercise/selectionexercise.cpp
+++ selectionexercise/selectionexercise.cpp
@@ -1,2 +1,4 @@
 #include <iostream>
 using namespace std;
+// 1.  Algoritma dibutuhkan untuk menyelesaikan suatu masalah karena algoritma adalah serangkaian langkah-langkah logis
+// yang terdefinisi dengan baik yang dapat diikuti untuk menyelesaikan suatu masalah atau mencapai suatu tujuan.
