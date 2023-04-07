ab200c46262b3db8e4a858aacc0070adc7cc7637
hi-lufz1 <latifufauzi@gmail.com>
2023-04-07 09:35:40 +0700

    no 4

--- selectionexercise/selectionexercise.cpp
+++ selectionexercise/selectionexercise.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 // 1.  Algoritma dibutuhkan untuk menyelesaikan suatu masalah karena algoritma adalah serangkaian langkah-langkah logis
 // yang terdefinisi dengan baik yang dapat diikuti untuk menyelesaikan suatu masalah atau mencapai suatu tujuan.
-// 2. Data struktur dapat diklasifikasikan menjadi dua jenis yaitu array dan linked list.
+// 2. Data struktur dapat diklasifikasikan menjadi dua jenis yaitu static (seperti array) dan dynamic (seperti linked list).
 // 3. Faktor-faktor yang mempengaruhi efisiensi suatu program algoritma meliputi kompleksitas algoritma, 
-//kecepatan prosesor komputer, ukuran input data dan jenis data
+// kecepatan prosesor komputer, ukuran input data dan jenis data
+// 4. Algoritma yang paling efisien untuk mengurutkan data yang ukurannya kecil adalah insertion sort.
+// Karena insertion sort memiliki kompleksitas waktu O(n^2) yang lebih baik daripada algoritma bubble sort atau selection sort pada ukuran data kecil.
