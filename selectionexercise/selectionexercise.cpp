b4bf3b3fb2523b906b912b5ef036a9f88e047323
hi-lufz1 <latifufauzi@gmail.com>
2023-04-07 09:28:09 +0700

    no 2

--- selectionexercise/selectionexercise.cpp
+++ selectionexercise/selectionexercise.cpp
@@ -2,3 +2,4 @@
 using namespace std;
 // 1.  Algoritma dibutuhkan untuk menyelesaikan suatu masalah karena algoritma adalah serangkaian langkah-langkah logis
 // yang terdefinisi dengan baik yang dapat diikuti untuk menyelesaikan suatu masalah atau mencapai suatu tujuan.
+// 2. Daata struktur dapt diklasifikasikan menjadi dua jenis yaitu array dan linked list.
