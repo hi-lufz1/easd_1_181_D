1b75fc1132d9f579534eccb5207241023627bf6d
hi-lufz1 <latifufauzi@gmail.com>
2023-04-07 10:00:53 +0700

    prosedur input

--- selectionexercise/selectionexercise.cpp
+++ selectionexercise/selectionexercise.cpp
@@ -14,4 +14,33 @@ using namespace std;
 int latif[101];
 int n;
 
+void input() {  
+    while (true)
+    {
+        cout << "Maksimal data array adalah 101";
+        cout << "\nMasukkan Jumlah Data pada Array : "; 
+        cin >> n;                                                      
+        
+
+        if (n <= 101) {                                                 
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 101 elemen. \n";   
+        }
+    }
+    cout << endl;                                                      
+    cout << "======================" << endl;                           
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
+
+
+    for (int i = 0; i < n; i++)   
+    {
+        cout << "Data ke-" << (i + 1) << ":"; 
+        cin >> latif[i];
+    }
+}
+
 
